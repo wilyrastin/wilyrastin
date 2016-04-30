13cb93cbb4a3e1cbff423490e3131ca98d1f9c69
wilyrastin <wilyrastin0098@gmail.com>
2016-04-30 18:28:09 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -224,7 +224,8 @@ function create_config( )
     "invite",
     "all",
     "leave_ban",
-    "admin"
+    "admin",
+    "welcome"
     },
     sudo_users = {205903314},--Sudo users
     disabled_channels = {},
